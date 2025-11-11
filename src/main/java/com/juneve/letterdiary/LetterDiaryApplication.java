88220f43831b14f4c92222bb289817def1ba63c4
방혜준 <kkngrs2@gmail.com>
2025-11-11 15:20:21 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/juneve/letterdiary/LetterDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.juneve.letterdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetterDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juneve/letterdiary/LetterDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juneve.letterdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
