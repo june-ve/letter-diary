2641fd61960a7a5221f9048256f09d48297cd4f3
방혜준 <kkngrs2@gmail.com>
2025-11-20 16:16:29 +0900

    feat: 비동기 처리 활성화를 위한 @EnableAsync 추가
    - LetterDiaryApplication 에 @EnableAsync 적용
    - @Async 기반의 비동기 메서드가 동작할 수 있는 환경 준비

--- src/main/java/com/juneve/letterdiary/LetterDiaryApplication.java
+++ src/main/java/com/juneve/letterdiary/LetterDiaryApplication.java
@@ -2,8 +2,10 @@ package com.juneve.letterdiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LetterDiaryApplication {
 
     public static void main(String[] args) {
